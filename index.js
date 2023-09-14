76bd75b8a4cb9ef4b49d44b1c03b804662cbe7b5
sonuparjapat <sahil992001parjapat@gmail.com>
2023-09-14 12:37:56 +0530

    changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { assignmentRouter } = require('./Controls/assignmentControl')
 const app=express()
 app.use(cors())
 app.use(express.json())
-// ||||||||||||||||||||||||||||||||||||||||||||||
+// |||||||||||||||||||||||||||
 app.get("/",async(req,res)=>{
     res.status(200).json({msg:"Welcome To The YakshAcademy Backend"})
 })
