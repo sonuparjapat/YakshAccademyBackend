0c755450e5151954ad8bb1f31b43d780f9f8d133
sonuparjapat <sahil992001parjapat@gmail.com>
2023-09-19 11:54:23 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
-
+//
 // |||||||||||||||||||||||||||
 app.get("/",async(req,res)=>{
     res.status(200).json({msg:"Welcome To The YakshAcademy Backend"})
