997c5b190d3e8af8eeb8032023ac541ea374ec34
sonuparjapat <sahil992001parjapat@gmail.com>
2023-10-07 13:59:21 +0530

    changes to registrationprocess

--- Controls/AdminControls.js
+++ Controls/AdminControls.js
@@ -3,6 +3,7 @@ const { AdminModel } = require("../Medels/AdminModel")
 const bcrypt = require('bcrypt');
 const AdminRouter=express.Router()
 var jwt = require('jsonwebtoken');
+const { loginmodel } = require("../Medels/AuthenticationModel");
 
 AdminRouter.post("/register",async(req,res)=>{
     const {name,Id,email,password}=req.body
@@ -45,5 +46,39 @@ AdminRouter.post("/login",async(req,res)=>{
     }else{
         res.status(400).json({msg:"No Data Found!!"})
     }
+})
+
+AdminRouter.post("/user/register",async(req,res)=>{
+
+  
+        const {email,password,name,type,field,unqId}=req.body
+        const data=await loginmodel.findOne({email})
+        const datawithid=await loginmodel.findOne({unqId})
+        if(email&&password&&name&&type&&field,unqId){
+    
+        
+        if(data){
+            res.status(400).json({msg:"This User is already registered"})
+        }else if(datawithid){
+            res.status(400).json({msg:"Given Id is Already Present "})}else{
+            try{
+                bcrypt.hash(password, 5, async(err, hash)=> {
+                    if(hash&&!err){
+    
+                        const data=new loginmodel({email,name,password:hash,type,field,unqId})
+                        await data.save()
+                        res.status(200).json({msg:"Registered Successfully"})
+                    }else{
+                        res.status(400).json({msg:err})
+                    }
+                    // Store hash in your password DB.
+                });
+            }catch(err){
+    res.status(400).json({msg:err})
+            }
+        }}else{
+            res.status(400).json({msg:"Please provide all the required details"})
+        }
+    
 })
 module.exports={AdminRouter}
--- Controls/loginstystem.js
+++ Controls/loginstystem.js
@@ -8,36 +8,7 @@ const authRouter=express.Router()
 // starting
 // authrouter
 
-authRouter.post("/register",async(req,res)=>{
-    const {email,password,name,type,field,unqId}=req.body
-    const data=await loginmodel.findOne({email})
-    const datawithid=await loginmodel.findOne({unqId})
-    if(email&&password&&name&&type&&field,unqId){
 
-    
-    if(data){
-        res.status(400).json({msg:"This User is already registered"})
-    }else if(datawithid){
-        res.status(400).json({msg:"Given Id is Already Present "})}else{
-        try{
-            bcrypt.hash(password, 5, async(err, hash)=> {
-                if(hash&&!err){
-
-                    const data=new loginmodel({email,name,password:hash,type,field,unqId})
-                    await data.save()
-                    res.status(200).json({msg:"Registered Successfully"})
-                }else{
-                    res.status(400).json({msg:err})
-                }
-                // Store hash in your password DB.
-            });
-        }catch(err){
-res.status(400).json({msg:err})
-        }
-    }}else{
-        res.status(400).json({msg:"Please provide all the required details"})
-    }
-})
 authRouter.post("/login",async(req,res)=>{
     const {email,password}=req.body
     const data=await loginmodel.findOne({email})
