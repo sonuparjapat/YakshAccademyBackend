7191b670e8b10325b4125f7ae4d805e038702e81
sonuparjapat <sahil992001parjapat@gmail.com>
2023-10-11 12:32:20 +0530

    backend admin

--- Controls/AdminControls.js
+++ Controls/AdminControls.js
@@ -31,7 +31,14 @@ AdminRouter.post("/login",async(req,res)=>{
     const {email,password}=req.body
     const admindata=await AdminModel.findOne({email})
     const alldata=await loginmodel.find()
-
+const students=await loginmodel.find({"type":"student"})
+const instructer=await loginmodel.find({"type":"instructer"})
+const frontendstudents=await loginmodel.find({"field":"frontend"})
+const backendstudents=await loginmodel.find({"field":"backend"})
+const fullstackstudents=await loginmodel.find({"field":"fullstack"})
+const frontendinstructers=await loginmodel.find({"type":"instructer","field":"frontend"})
+const backendinstructers=await loginmodel.find({"type":"instructer","field":"backend"})
+const fullstackinstructers=await loginmodel.find({"type":"instructer","field":"fullstack"})
     // console.log(email)
     // console.log(admindata)
     if(admindata){
@@ -39,7 +46,10 @@ AdminRouter.post("/login",async(req,res)=>{
             bcrypt.compare(password, admindata.password, function(err, result) {
               if(result){
                 var token = jwt.sign({ adminId:admindata._id }, 'masai',{ expiresIn: '1h' });
-                res.status(200).json({msg:"Login Successfully",adminname:admindata.name,adminemail:admindata.email,admintoken:token,adminId:admindata.Id,alldata:alldata.length})
+                res.status(200).json({msg:"Login Successfully",adminname:admindata.name,adminemail:admindata.email,admintoken:token,adminId:admindata.Id,
+                alldata:alldata.length,"studentscount":students.length,instructerscount:instructer.length,"frontendstudentscount":frontendstudents.length,backendstudentscount:backendstudents.length,fullstackstudentscount:fullstackstudents.length,
+            frontendinstructerscount:frontendinstructers.length,backendinstructerscount:backendinstructers.length,fullstackinstructerscount:fullstackinstructers.length,
+      students,instructer,frontendinstructers,backendinstructers,fullstackinstructers,frontendstudents,backendstudents,fullstackstudents})
               }else{
                 res.status(400).json({msg:"Password Mismatch!!"})
               }
